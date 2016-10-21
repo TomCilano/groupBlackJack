c9698bdf040050dadaff296325f575c9ed022da9
nathaniel ellsworth <nathanielellsworth@nathaniels-MacBook-Air.local>
2016-10-21 11:46:54 -0400

    thisCommit

--- src/Java/com/ironyard/data/Cards.java
+++ src/Java/com/ironyard/data/Cards.java
@@ -1,26 +1,70 @@
 package com.ironyard.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Tom on 10/20/16.
  */
 public class Cards {
-    public int value;
-    private enum Suite;
+    public enum SUIT {
+        HEARTS,
+        SPADES,
+        DIAMONDS,
+        CLUBS
+    }
 
-    public Cards(int value) {
+    private int value;
+    private String name;
+    private Player owner;
+    private SUIT suit;
+
+    public Cards(int value, String name, Player owner, SUIT suit){
         this.value = value;
+        this.name = name;
+        this.owner = owner;
+        this.suit = suit;
     }
 
-    public int getValue() {
-        return value;
+    public static List<Cards> createDeck() {
+        List<Cards> deck = new ArrayList<Cards>();
+        for (SUIT aSuit : SUIT.values()) {
+            // cards 2 - 10
+            for (int i = 2; i <= 10; i++) {
+                String aCardName = String.format("%s of %s", i, aSuit);
+                deck.add(new Cards(i, aCardName, null, aSuit));
+            }
+            //card faces
+            deck.add(new Cards(11, String.format("Ace of %s", aSuit), null, aSuit));
+            deck.add(new Cards(10, String.format("King of %s", aSuit), null, aSuit));
+            deck.add(new Cards(10, String.format("Queen of %s", aSuit), null, aSuit));
+            deck.add(new Cards(10, String.format("Jack of %s", aSuit), null, aSuit));
+            deck.add(new Cards(10, String.format("10 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(9, String.format("9 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(8, String.format("8 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(7, String.format("7 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(6, String.format("6 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(5, String.format("5 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(4, String.format("4 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(3, String.format("3 of %s", aSuit), null, aSuit));
+            deck.add(new Cards(2, String.format("2 of %s", aSuit), null, aSuit));
+        }
+        return deck;
     }
 
-    public void setValue(int value) {
-        this.value = value;
-    }
+    //getters and setters
 
-    {
+    public int getValue() {return value;}
+    public void setValue(int value) {this.value = value;}
 
-    }
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
 
+    public Player getOwner() {return owner;}
+    public void setOwner(Player owner) {this.owner = owner;}
+
+    public SUIT getSuit() {return suit;}
+    public void setSuit(SUIT suit) {this.suit = suit;}
 }
+
+
--- src/Java/com/ironyard/data/Game.java
+++ src/Java/com/ironyard/data/Game.java
@@ -1,33 +1,148 @@
 package com.ironyard.data;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Tom on 10/20/16.
  */
 public class Game {
-    public static void main (String[] args){
+    private List<Player> players;
+    private List<Cards> cardsInPlay;
+    private List<Cards> startingDeck;
+    int turns = 0;
 
-
-        // game starts
-
-
-        // Deal cards{
-        //----->deck shuffles
-        // ---->player gets two cards
-        // ---->dealer gets one}
-        //
-        //  Player Turn
-        //----->player can hit or stay
-        //----->if player hits he is dealt one card
-        //----->if player goes over 21 the game is over.
-        //----->if he stays dealers turn
-        //
-        //Dealer Turn
-        //----->dealer draws cards until >= 17
-        //----->evaluate the scores, closest to 21 but not over wins. If tie dealer wins
-
-        //play again
-
-
-        //Nate test
+    public List<Player> getPlayers() {
+        return players;
     }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Cards> getCardsInPlay() {
+        return cardsInPlay;
+    }
+
+    public void setCardsInPlay(List<Cards> cardsInPlay) {
+        this.cardsInPlay = cardsInPlay;
+    }
+
+    public List<Cards> getStartingDeck() {
+        return startingDeck;
+    }
+
+    public void setStartingDeck(List<Cards> startingDeck) {
+        this.startingDeck = startingDeck;
+    }
+
+    public int getTurns() {
+        return turns;
+    }
+
+    public void setTurns(int turns) {
+        this.turns = turns;
+    }
+
+    public static void main(String[] args) {
+        Game g = new Game();
+        g.initGame(4);
+        g.dealOutAllCards();
+        Player winner = g.startGame();
+
+        System.out.printf("\n\n%s is the Winner! In %s turns\n", winner.getName(), g.turns);
+    }
+
+    // game starts
+    public Player startGame(){
+        Player winner = null;
+        while(winner == null){
+            playRound();
+            winner = getWinner();
+        }
+        return winner;
+    }
+
+    public void playRound(){
+        // each player puts down top card
+        for(Player p:players){
+            cardsInPlay.add(p.playCard());
+        }
+        turns++;
+
+        // see who won
+        Cards highest = null;
+        for(Cards c:cardsInPlay){
+            if(highest == null){
+                highest = c;
+            }else if(highest.getValue() < c.getValue()){
+                highest = c;
+            }
+        }
+
+        // winner gets all played cards
+        highest.getOwner().acceptWonCards(cardsInPlay);
+        cardsInPlay.clear();
+
+    }
+
+    // Deal cards{
+    public void dealOutAllCards(){
+        // go through staringDeck
+        int playerTurn = 0;
+        for(Cards c: startingDeck) {
+            // give each player 1 card (repeat)
+            int pos = playerTurn % players.size();
+            players.get(pos).acceptCardForPlay(c);
+            playerTurn++;
+        }
+
+    }
+    //----->deck shuffles
+    public static void shuffle(List<Cards> shuffleMePlease){
+        // pick random number
+        Collections.shuffle(shuffleMePlease);
+    }
+
+    public void initGame(int numberOfPlayers){
+        // set all lists to new empty lists
+        turns = 0;
+        players = new ArrayList<players>();
+        cardsInPlay = new ArrayList<cardsInPlay>();
+        startingDeck = new ArrayList<startingDeck>();
+
+        // create x players add to list of players
+        for(int i=0; i<numberOfPlayers; i++){
+            players.add(new Player("Player #"+i));
+        }
+
+        // init deck of cards
+        startingDeck = Cards.createDeck();
+
+        // shuffle deck of cards
+        shuffle(startingDeck);
+
+    }
+
+    // ---->player gets two cards
+    // ---->dealer gets one}
+    //
+    //  Player Turn
+    //----->player can hit or stay
+    //----->if player hits he is dealt one card
+    //----->if player goes over 21 the game is over.
+    //----->if he stays dealers turn
+    //
+    //Dealer Turn
+    //----->dealer draws cards until >= 17
+    //----->evaluate the scores, closest to 21 but not over wins. If tie dealer wins
+
+    //play again
+
+
+
+
+
 }
+
